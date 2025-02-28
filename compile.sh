298e9e8b8811e206e5af152477b3db9896be44ef
Taj Gillin <tajgillin@gmail.com>
2025-02-28 15:15:40 -0500

    Optimized compiling

--- compile.sh
+++ compile.sh
@@ -5,4 +5,4 @@
 ########################################
 
 # Compile C++ code with g++
-g++ -std=c++17 -Wall -Ofast -I src/ src/main.cpp src/dimacs_parser.cpp src/solvers/dpll.cpp -o dpll_solver
+g++ -std=c++17 -Wall -Ofast -flto -march=native -funroll-loops -fomit-frame-pointer -I src/ src/main.cpp src/dimacs_parser.cpp src/solvers/dpll.cpp -o dpll_solver
